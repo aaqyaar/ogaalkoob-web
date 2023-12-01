c41ca7899735249577fe526522e72328fb429924
abdizamedmo <abdizamedmo@gmail.com>
2023-12-01 17:09:47 +0300

    Books api added

---
+++ app/api/books/[id]/route.ts
@@ -0,0 +1,88 @@
+import { isAuthenticated } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import { NextRequest, NextResponse } from "next/server";
+
+interface BookParams extends Params {
+  id: string;
+}
+
+export async function GET(req: NextRequest, { params }: BookParams) {
+  try {
+    await isAuthenticated(req, ["ADMIN", "SUBSCRIBER"]);
+
+    const book = await prisma.book.findUnique({
+      where: { id: params.id },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        price: true,
+        author: true,
+        photos: true,
+        publishedDate: true,
+        isbn: true,
+        genre: {
+          select: { id: true, name: true },
+        },
+      },
+    });
+
+    if (!book) {
+      return NextResponse.json(
+        { message: "Book not found" },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        ...book,
+      },
+      { status: 200 }
+    );
+  } catch (err) {
+    const error = err as Error;
+    return NextResponse.json(
+      { message: error.message || error.toString() },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function PUT(req: NextRequest, { params }: BookParams) {
+  const body = await req.json();
+  try {
+    await isAuthenticated(req);
+
+    const book = await prisma.book.update({
+      where: { id: params.id },
+      data: {
+        ...body,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: "Book updated successfully",
+        data: book,
+      },
+      { status: 200 }
+    );
+  } catch (err) {
+    const error = err as Error;
+    const formattedError = error.message.includes("Unique constraint failed")
+      ? "Book already exists"
+      : error.message || error.toString();
+    return NextResponse.json(
+      { message: formattedError },
+      {
+        status: 500,
+      }
+    );
+  }
+}
---
+++ app/api/books/route.ts
@@ -0,0 +1,169 @@
+import { isAuthenticated } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { bookSchema } from "@/validations/books";
+import { Genre, Prisma } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import * as z from "zod";
+
+export async function GET(req: NextRequest) {
+  try {
+    await isAuthenticated(req, ["ADMIN", "SUBSCRIBER"]);
+
+    const { searchParams } = new URL(req.url);
+    const page = parseInt(searchParams.get("page") as string) || 1;
+    const limit = parseInt(searchParams.get("limit") as string) || 5;
+    const q = searchParams.get("q") as string;
+
+    const skip = (page - 1) * limit;
+    const qNumber = parseFloat(q);
+    const isNumber = !isNaN(qNumber);
+
+    const query = q
+      ? {
+          OR: [
+            {
+              title: {
+                contains: q,
+                mode: Prisma.QueryMode.insensitive,
+              },
+            },
+            {
+              author: {
+                contains: q,
+                mode: Prisma.QueryMode.insensitive,
+              },
+            },
+            {
+              description: {
+                contains: q,
+                mode: Prisma.QueryMode.insensitive,
+              },
+            },
+            {
+              isbn: {
+                contains: q,
+                mode: Prisma.QueryMode.insensitive,
+              },
+            },
+
+            ...(isNumber ? [{ price: { equals: qNumber } }] : []),
+            {
+              genre: {
+                some: {
+                  name: {
+                    contains: q,
+                    mode: Prisma.QueryMode.insensitive,
+                  },
+                },
+              },
+            },
+          ],
+        }
+      : {};
+
+    const [books, total] = await Promise.all([
+      prisma.book.findMany({
+        skip,
+        take: limit,
+        where: {
+          ...query,
+        },
+        select: {
+          id: true,
+          title: true,
+          description: true,
+          price: true,
+          author: true,
+          photos: true,
+          publishedDate: true,
+          isbn: true,
+          genre: {
+            select: { id: true, name: true },
+          },
+        },
+      }),
+      prisma.book.count({
+        where: {
+          ...query,
+        },
+      }),
+    ]);
+
+    const pages = Math.ceil(total / limit);
+
+    return NextResponse.json(
+      {
+        startIndex: skip + 1,
+        endIndex: skip + books.length,
+        count: books.length,
+        currentPage: page,
+        numberOfPages: pages,
+        data: books,
+      },
+      { status: 200 }
+    );
+  } catch (err) {
+    const error = err as Error;
+    return NextResponse.json(
+      { message: error.message || error.toString() },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  try {
+    await isAuthenticated(req);
+
+    bookSchema.parse(body);
+
+    let genre: Genre["id"][] = [];
+
+    if (body.genre) {
+      genre = Array.isArray(body.genre) ? body.genre : [body.genre];
+    }
+
+    genre = genre?.filter(Boolean);
+
+    const book = await prisma.book.create({
+      data: {
+        ...body,
+        genre: {
+          connect: genre?.map((pre: Genre["id"]) => ({ id: pre })),
+        },
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: "Book created successfully",
+        data: book,
+      },
+      { status: 201 }
+    );
+  } catch (err) {
+    const error = err as Error;
+
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { errors: error.flatten().fieldErrors },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const formattedError = error.message.includes("Unique constraint failed")
+      ? "Book already exists"
+      : error.message || error.toString();
+    return NextResponse.json(
+      { message: formattedError },
+      {
+        status: 500,
+      }
+    );
+  }
+}
