efe980b59ae6ec9f6b4adce34d203cc0dfa1ac46
abdizamedmo <abdizamedmo@gmail.com>
2023-12-06 12:18:40 +0300

    added status for tracking async operations

--- models/auth-store.ts
+++ models/auth-store.ts
@@ -4,10 +4,14 @@ import { create } from "zustand";
 import api from "@/services/api";
 import { persist } from "zustand/middleware";
 
+export type Status = "idle" | "pending" | "fail" | "done";
+
 type AuthStore = {
   token: string;
   profile: IUser;
   isAuthenticated: boolean;
+  status: Status;
+  setStatus: (status: Status) => void;
   login: (email: string, password: string) => Promise<ILoginResponse>;
   logout: () => void;
   getMe: () => Promise<IUser>;
@@ -17,24 +21,33 @@ export const useAuthStore = create(
   persist<AuthStore>(
     (set) => ({
       token: "",
+      status: "idle",
       isAuthenticated: false,
       profile: {} as IUser,
       login: async (email: string, password: string) => {
+        set({ status: "pending" });
         const { token } = await api.post<ILoginResponse>("/auth/login", {
           email,
           password,
         });
 
-        set({ token, isAuthenticated: true });
+        set({ token, isAuthenticated: true, status: "done" });
         return { token };
       },
+      setStatus: (status: Status) => set({ status }),
       getMe: async () => {
+        set({ status: "pending" });
         const data = await api.get<IUser>("/auth/me");
-        set({ profile: data });
+        set({ profile: data, status: "done" });
         return data;
       },
       logout: () =>
-        set({ token: "", profile: {} as IUser, isAuthenticated: false }),
+        set({
+          token: "",
+          profile: {} as IUser,
+          isAuthenticated: false,
+          status: "idle",
+        }),
     }),
     {
       name: "authStore",
