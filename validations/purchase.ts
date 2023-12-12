875e54c6ad6b8157ad72e0fc8dfdc7422f75843e
abdizamedmo <abdizamedmo@gmail.com>
2023-12-12 10:39:47 +0300

    Validation zod updates

--- validations/purchase.ts
+++ validations/purchase.ts
@@ -10,4 +10,15 @@ export const purchaseSchema = z.object({
     .string()
     .min(2, "Phone number is required and must be at least 2 characters"),
   paymentMethod: z.enum(["CARD", "CASH", "MMT"]),
+  purchaseDate: z.string(),
+});
+
+export const purchaseStatusSchema = z.object({
+  status: z.enum(["COMPLETED", "CANCELLED", "PENDING"]),
+  userId: z
+    .string()
+    .min(2, "User ID is required and must be at least 2 characters"),
+  purchaseId: z
+    .string()
+    .min(2, "Purchase ID is required and must be at least 2 characters"),
 });
--- validations/user.ts
+++ validations/user.ts
@@ -1,11 +1,24 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z
+  username: z
     .string({
-      required_error: "Email is required and must be a valid email address",
+      required_error:
+        "Username is required and must be a valid email address or phone number",
     })
-    .email(),
+    .refine(
+      (val) => {
+        // If the value includes '@', validate as an email
+        if (val.includes("@")) {
+          return z.string().email().safeParse(val).success;
+        }
+        return z.string().min(6).max(13).safeParse(val).success;
+      },
+      {
+        message: "Invalid username. It must be a valid email or phone number.",
+      }
+    ),
+
   password: z
     .string({
       required_error:
@@ -25,8 +38,26 @@ export const registerSchema = z.object({
       required_error:
         "Password is required and must be at least 6 characters long",
     })
-    .min(6, "Password is required and must be at least 6 characters long"),
+    .min(6, "Password is required and must be at least 6 characters long")
+    .optional(),
   name: z.string().min(2, "Name is required and must be at least 2 characters"),
+  phone: z
+    .string({
+      required_error:
+        "Phone number is required and must be a valid phone number",
+    })
+    .min(6, "Phone number is required and must be at least 6 characters long"),
+  status: z.optional(
+    z.string().min(2, "Status is required and must be at least 2 characters")
+  ),
+
+  role: z.optional(
+    z
+      .string({
+        required_error: "Role is required",
+      })
+      .min(2, "Role is required and must be at least 2 characters")
+  ),
 });
 
 export const resetPasswordSchema = z.object({
