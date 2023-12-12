b2130edf799b856511cb9e105a1d472333dc7df0
abdizamedmo <abdizamedmo@gmail.com>
2023-12-12 10:34:44 +0300

    API's swapped

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -2,140 +2,9 @@ import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import { isAuthenticated } from "@/lib/auth";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-import { Book } from "@prisma/client";
-import { purchaseSchema } from "@/validations/purchase";
-import * as z from "zod";
 
 interface PurchaseParams extends Params {
   userId: string;
-}
-
-export async function POST(req: NextRequest, { params }: PurchaseParams) {
-  const body = await req.json();
-  try {
-    const user = await isAuthenticated(req, ["ADMIN", "SUBSCRIBER"]);
-    console.log(user);
-    if (user.id !== params.userId) {
-      return NextResponse.json(
-        { message: "You are not authorized to perform this action" },
-        {
-          status: 403,
-        }
-      );
-    }
-
-    const books = body.books;
-
-    // Validate the body
-    purchaseSchema.parse(body);
-
-    if (Array.isArray(books)) {
-      for (const id of books) {
-        const bookExists = await prisma.book.findFirst({
-          where: {
-            id,
-          },
-        });
-
-        if (!bookExists) {
-          return NextResponse.json(
-            { message: "Book not found" },
-            {
-              status: 404,
-            }
-          );
-        }
-      }
-    }
-    // Check if the user has already purchased the book
-    const purchaseExists = await prisma.purchase.findFirst({
-      where: {
-        userId: params.userId,
-        books: {
-          some: {
-            id: {
-              in: books.map((book: Book["id"]) => book),
-            },
-          },
-        },
-      },
-    });
-
-    if (purchaseExists) {
-      return NextResponse.json(
-        { message: "You have already purchased this book" },
-        {
-          status: 409,
-        }
-      );
-    }
-
-    // Check if the body.amount is equal to the total price of the books
-    const total = await prisma.book.findMany({
-      where: {
-        id: {
-          in: books.map((book: Book["id"]) => book),
-        },
-      },
-      select: {
-        price: true,
-      },
-    });
-
-    const totalPrice = total.reduce((acc, curr) => acc + curr.price, 0);
-
-    if (totalPrice !== body.amount) {
-      return NextResponse.json(
-        { message: "Amount is not equal to the total price of the books" },
-        {
-          status: 400,
-        }
-      );
-    }
-
-    // TODO: Make the payment here
-    const purchase = await prisma.purchase.create({
-      data: {
-        amount: body.amount,
-        purchaseDate: new Date(),
-        userId: params.userId,
-        paymentMethod: body.paymentMethod,
-        phoneNumber: body.phoneNumber,
-        books: {
-          connect: books?.map((pre: Book["id"]) => ({ id: pre })),
-        },
-      },
-      include: {
-        books: true,
-      },
-    });
-
-    return NextResponse.json(
-      {
-        message: "Purchase successful",
-        data: purchase,
-      },
-      { status: 201 }
-    );
-  } catch (err) {
-    const error = err as Error;
-
-    if (error instanceof z.ZodError) {
-      return NextResponse.json(
-        { errors: error.flatten().fieldErrors },
-        {
-          status: 400,
-        }
-      );
-    }
-
-    return NextResponse.json(
-      { message: error.message || error.toString() },
-      {
-        status: 500,
-      }
-    );
-  }
 }
 
 export async function GET(req: NextRequest, { params }: PurchaseParams) {
@@ -149,14 +18,23 @@ export async function GET(req: NextRequest, { params }: PurchaseParams) {
 
     const skip = (page - 1) * limit;
 
-    const purchases = await prisma.purchase.findMany({
-      take: limit,
-      skip,
-      where: {
-        userId: params.userId,
-      },
-      include: { books: true },
-    });
+    const [purchases, total] = await Promise.all([
+      await prisma.purchase.findMany({
+        take: limit,
+        skip,
+        where: {
+          userId: params.userId,
+          status: "COMPLETED",
+        },
+        include: { books: true },
+      }),
+      await prisma.purchase.count({
+        where: {
+          userId: params.userId,
+          status: "COMPLETED",
+        },
+      }),
+    ]);
 
     return NextResponse.json(
       {
@@ -165,6 +43,7 @@ export async function GET(req: NextRequest, { params }: PurchaseParams) {
         count: purchases.length,
         currentPage: page,
         data: purchases,
+        numberOfPages: Math.ceil(total / limit),
       },
       { status: 200 }
     );
--- app/api/purchases/route.ts
+++ app/api/purchases/route.ts
@@ -1,6 +1,138 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import { isAuthenticated } from "@/lib/auth";
+import { Book } from "@prisma/client";
+import { purchaseSchema } from "@/validations/purchase";
+import * as z from "zod";
+
+export async function POST(req: NextRequest) {
+  const body = (await req.json()) as z.infer<typeof purchaseSchema>;
+  try {
+    const { user } = await isAuthenticated(req, ["ADMIN", "SUBSCRIBER"]);
+
+    if (user.id !== body.userId) {
+      return NextResponse.json(
+        { message: "You are not authorized to perform this action" },
+        {
+          status: 403,
+        }
+      );
+    }
+
+    const books = body.books as z.infer<typeof purchaseSchema>["books"];
+
+    // Validate the body
+    purchaseSchema.parse(body);
+
+    if (Array.isArray(books)) {
+      for (const id of books) {
+        const bookExists = await prisma.book.findFirst({
+          where: {
+            id,
+          },
+        });
+
+        if (!bookExists) {
+          return NextResponse.json(
+            { message: "Book not found" },
+            {
+              status: 404,
+            }
+          );
+        }
+      }
+    }
+    // Check if the user has already purchased the book
+    const purchaseExists = await prisma.purchase.findFirst({
+      where: {
+        userId: body.userId,
+        books: {
+          some: {
+            id: {
+              in: books.map((book: Book["id"]) => book),
+            },
+          },
+        },
+      },
+    });
+
+    if (purchaseExists) {
+      return NextResponse.json(
+        { message: "You have already purchased this book" },
+        {
+          status: 409,
+        }
+      );
+    }
+
+    // Check if the body.amount is equal to the total price of the books
+    const total = await prisma.book.findMany({
+      where: {
+        id: {
+          in: books.map((book: Book["id"]) => book),
+        },
+      },
+      select: {
+        price: true,
+      },
+    });
+
+    const totalPrice = total.reduce((acc, curr) => acc + curr.price, 0);
+
+    if (totalPrice !== body.amount) {
+      return NextResponse.json(
+        { message: "Amount is not equal to the total price of the books" },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    // TODO: Make the payment here
+    const purchase = await prisma.purchase.create({
+      data: {
+        amount: body.amount,
+        purchaseDate: new Date(),
+        userId: body.userId,
+        paymentMethod: body.paymentMethod,
+        phoneNumber: body.phoneNumber,
+        status: totalPrice === 0 ? "COMPLETED" : "PENDING",
+        books: {
+          connect: books?.map((pre: Book["id"]) => ({ id: pre })),
+        },
+      },
+      include: {
+        books: true,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: "Purchase successful",
+        data: purchase,
+      },
+      { status: 201 }
+    );
+  } catch (err) {
+    const error = err as Error;
+
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { errors: error.flatten().fieldErrors },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      { message: error.message || error.toString() },
+      {
+        status: 500,
+      }
+    );
+  }
+}
 
 export async function GET(req: NextRequest) {
   try {
@@ -13,11 +145,23 @@ export async function GET(req: NextRequest) {
 
     const skip = (page - 1) * limit;
 
-    const purchases = await prisma.purchase.findMany({
-      take: limit,
-      skip,
-      include: { books: true, user: true },
-    });
+    const [purchases, total] = await Promise.all([
+      await prisma.purchase.findMany({
+        take: limit,
+        skip,
+        include: {
+          books: true,
+          user: {
+            include: {
+              role: true,
+            },
+          },
+        },
+      }),
+      await prisma.purchase.count(),
+    ]);
+
+    const pages = Math.ceil(total / limit);
 
     return NextResponse.json(
       {
@@ -26,6 +170,7 @@ export async function GET(req: NextRequest) {
         count: purchases.length,
         currentPage: page,
         data: purchases,
+        numberOfPages: pages,
       },
       { status: 200 }
     );
