a26a24b5d1b7cb700b40784280b80385f8f79c85
abdizamedmo <abdizamedmo@gmail.com>
2023-12-12 10:35:55 +0300

    registeration added in auth store

--- models/auth-store.ts
+++ models/auth-store.ts
@@ -13,6 +13,13 @@ type AuthStore = {
   status: Status;
   setStatus: (status: Status) => void;
   login: (email: string, password: string) => Promise<ILoginResponse>;
+  register: (
+    name: string,
+    email: string,
+    phone: string,
+    password: string
+  ) => Promise<ILoginResponse>;
+
   logout: () => void;
   getMe: () => Promise<IUser>;
 };
@@ -24,22 +31,49 @@ export const useAuthStore = create(
       status: "idle",
       isAuthenticated: false,
       profile: {} as IUser,
-      login: async (email: string, password: string) => {
+      login: async (username: string, password: string) => {
         set({ status: "pending" });
         const { token } = await api.post<ILoginResponse>("/auth/login", {
-          email,
+          username,
           password,
         });
 
         set({ token, isAuthenticated: true, status: "done" });
         return { token };
       },
+      register: async (
+        name: string,
+        email: string,
+        phone: string,
+        password: string
+      ) => {
+        try {
+          set({ status: "pending" });
+          const { token } = await api.post<ILoginResponse>("/auth/register", {
+            name,
+            email,
+            phone,
+            password,
+          });
+
+          set({ status: "done" });
+          return { token };
+        } catch (error) {
+          set({ status: "fail" });
+          throw error;
+        }
+      },
       setStatus: (status: Status) => set({ status }),
       getMe: async () => {
-        set({ status: "pending" });
-        const data = await api.get<IUser>("/auth/me");
-        set({ profile: data, status: "done" });
-        return data;
+        try {
+          set({ status: "pending" });
+          const data = await api.get<IUser>("/auth/me");
+          set({ profile: data, status: "done" });
+          return data;
+        } catch (error) {
+          set({ status: "fail" });
+          throw error;
+        }
       },
       logout: () =>
         set({
